e5f7fd8a7b4f3b381e11113537d036dd8771328d
jseto <jseto@jasvisio.com>
2015-01-04 21:13:12 +0100

    header e2e tests

--- test/client/views/header/header.e2e-spec.js
+++ test/client/views/header/header.e2e-spec.js
@@ -1,12 +1,45 @@
 'use strict';
 
-describe('HeaderCtrl', function(){
+describe('Header Navigation', function(){
 
-	beforeEach(function(){
-		browser.get('index.html');
+	// beforeAll(function(){
+	// });
+
+	it('Should navigate to Home page', function () {
+		browser.get('/');
+		expect( browser.getTitle() ).toBe('Home - MyApp');
+		expect( browser.getLocationAbsUrl() ).toBe('/home');
+		expect( element( by.id('homeTab') ).getAttribute('class') ).toMatch(/\bactive\b/);
+		expect( element( by.id('contactTab') ).getAttribute('class') ).not.toMatch(/\bactive\b/);
+		expect( element( by.id('loginTab') ).getAttribute('class') ).not.toMatch(/\bactive\b/);
+		console.log(element( by.id('contactTab') ).getAttribute('class'));
 	});
 
-	it('Test array key', function () {
+	it('Should navigate to Contact by clicking', function() {
+		element( by.id('contactTab') ).click();
+		expect( browser.getTitle() ).toBe('Contact - MyApp');
+		expect( browser.getLocationAbsUrl() ).toBe('/contact');
+		expect( element( by.id('homeTab') ).getAttribute('class') ).not.toMatch(/\bactive\b/);
+		expect( element( by.id('contactTab') ).getAttribute('class') ).toMatch(/\bactive\b/);
+		expect( element( by.id('loginTab') ).getAttribute('class') ).not.toMatch(/\bactive\b/);
+	});
+
+	it('Should navigate to Home by clicking', function() {
+		element( by.id('homeTab') ).click();
 		expect( browser.getTitle() ).toBe('Home - MyApp');
+		expect( browser.getLocationAbsUrl() ).toBe('/home');
+		expect( element( by.id('homeTab') ).getAttribute('class') ).toMatch(/\bactive\b/);
+		expect( element( by.id('contactTab') ).getAttribute('class') ).not.toMatch(/\bactive\b/);
+		expect( element( by.id('loginTab') ).getAttribute('class') ).not.toMatch(/\bactive\b/);
+	});
+
+	it('Should navigate to Signup by clicking on popup panel', function() {
+		element( by.id('loginTab') ).click();
+		element( by.id('goSignup') ).click();
+		expect( browser.getTitle() ).toBe('Signup - MyApp');
+		expect( browser.getLocationAbsUrl() ).toBe('/signup');
+		expect( element( by.id('homeTab') ).getAttribute('class') ).not.toMatch(/\bactive\b/);
+		expect( element( by.id('contactTab') ).getAttribute('class') ).not.toMatch(/\bactive\b/);
+		expect( element( by.id('loginTab') ).getAttribute('class') ).not.toMatch(/\bactive\b/);
 	});
 });
